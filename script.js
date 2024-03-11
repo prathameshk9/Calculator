82bab68a6dc05ed23d5ad14cd1207164b1155c4f
prathameshk9 <prathameshkulkarni2576@gmail.com>
2024-03-11 15:28:47 +0530

    Calculator 1st commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// CALCULATOR PROGRAMME
+const display = document.getElementById("display");
+function appendToDisplay(input){
+   display.value += input;
+}
+ 
+function calculate(){
+    try {
+    display.value = eval(display.value);
+    }
+    catch{
+        display.value = "error"
+    }
+}
+function clearDisplay(){
+        display.value="";
+}
